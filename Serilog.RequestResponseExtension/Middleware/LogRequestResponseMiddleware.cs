1f1d1a89b2d32154a6545ed342d131ea9f2e2c62
Frederick Frigieri <fred@ezconet.com.br>
2021-01-18 22:38:03 -0300

    ajuste no middleware

--- Serilog.RequestResponseExtension/Middleware/LogRequestResponseMiddleware.cs
+++ Serilog.RequestResponseExtension/Middleware/LogRequestResponseMiddleware.cs
@@ -22,31 +22,25 @@ namespace Serilog.RequestResponseExtension.Middleware
         {
             context.Request.EnableBuffering();
 
+            var originalRequestBody = context.Request.Body;
             var buffer = new byte[Convert.ToInt32(context.Request.ContentLength)];
-
             await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
-
             var requestBody = Encoding.UTF8.GetString(buffer);
+            originalRequestBody.Seek(0, SeekOrigin.Begin);
+            context.Request.Body = originalRequestBody;
+
             var requestQuerystring = context.Request.QueryString;
-
-            context.Request.Body.Seek(0, SeekOrigin.Begin);
-
-            var originalBodyStream = context.Response.Body;
             var dataInicial = DateTime.Now;
 
             using (var responseBodyMemoryStream = new MemoryStream())
             {
+                var originalResponseBody = context.Response.Body;
                 context.Response.Body = responseBodyMemoryStream;
-
                 await _next(context);
-
                 context.Response.Body.Seek(0, SeekOrigin.Begin);
-
                 var responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
-
                 context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-                await responseBodyMemoryStream.CopyToAsync(originalBodyStream);
+                await responseBodyMemoryStream.CopyToAsync(originalResponseBody);
 
                 Log.ForContext("RequestBody", requestBody)
                     .ForContext("RequestHeaders", context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()), destructureObjects: true)
